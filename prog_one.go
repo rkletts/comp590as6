eb0dddf127e86ae3ad65e7041dfc82530b735ccf
Reese Letts <111404863+rkletts@users.noreply.github.com>
2025-03-20 20:17:04 -0400

    Update

---
+++ prog_one.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"math/big"
+	"time"
+)
+
+func power_h(n, p int64, res *big.Int) {
+	if p == 0 {
+		fmt.Printf("Result: %v\n", res)
+		return
+	}
+
+	nextRes := new(big.Int).Mul(res, big.NewInt(n))
+	go power_h(n, p-1, nextRes)
+}
+
+func power(n, p int64) {
+	go power_h(n, p, big.NewInt(1))
+
+}
+
+func main() {
+	n := int64(2)
+	p := int64(3)
+	power(n, p)
+
+	time.Sleep(3 * time.Second)
+
+}
